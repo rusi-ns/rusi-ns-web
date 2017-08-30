bae88c1c3678ad99a780a3a759a15cad692a7a62
RUSI-NS CIO <cio.rusi.ns@gmail.com>
2017-08-30 08:33:55 -0700

    Updated Patron footer.

--- wp-content/themes/colormag-child/functions.php
+++ wp-content/themes/colormag-child/functions.php
@@ -18,7 +18,8 @@ add_action( 'wp_enqueue_scripts', 'rusi_enqueue_styles' );
 function colormag_footer_copyright() {
    $site_link = '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" ><span>' . get_bloginfo( 'name', 'display' ) . '</span></a>';
 
-   $default_footer_value = sprintf( __( 'Patron: His Honour Brigadier-General the Honourable J. J. Grant, CMM, ONS, CD (Retd), Lieutenant Governor of Nova Scotia.<br/> Copyright &copy; %1$s %2$s. All rights reserved.', 'colormag' 
+   $default_footer_value = sprintf( __( 'Patron: His Honour, The Honourable Arthur J. LeBlanc, ONS, QC, Lieutenant Governor of Nova Scotia, Lieutenant Governor of Nova Scotia.<br/> Copyright &copy; %1$s %2$s. All rights 
+reserved.', 'colormag' 
 ), 
 date( 'Y' ), $site_link );
 
