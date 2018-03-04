d381506599828c36eac6771004d5def6b204cc99
RUSI-NS CIO <cio.rusi.ns@gmail.com>
2018-03-04 06:36:02 -0700

    Fixed typo in footer.

--- wp-content/themes/colormag-child/functions.php
+++ wp-content/themes/colormag-child/functions.php
@@ -18,7 +18,7 @@ add_action( 'wp_enqueue_scripts', 'rusi_enqueue_styles' );
 function colormag_footer_copyright() {
    $site_link = '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" ><span>' . get_bloginfo( 'name', 'display' ) . '</span></a>';
 
-   $default_footer_value = sprintf( __( 'Patron: His Honour, The Honourable Arthur J. LeBlanc, ONS, QC, Lieutenant Governor of Nova Scotia, Lieutenant Governor of Nova Scotia.<br/> Copyright &copy; %1$s %2$s. All rights 
+   $default_footer_value = sprintf( __( 'Patron: His Honour, The Honourable Arthur J. LeBlanc, ONS, QC, Lieutenant Governor of Nova Scotia.<br/> Copyright &copy; %1$s %2$s. All rights 
 reserved.', 'colormag' 
 ), 
 date( 'Y' ), $site_link );
